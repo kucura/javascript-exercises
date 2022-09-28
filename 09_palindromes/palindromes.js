98d45617fe38e9bd6a10bf0d606052c8dcf160c5
kucura <rukicu.gorski@gmail.com>
2022-09-28 15:41:53 +0200

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,14 @@
 const palindromes = function (string) {
-    let newString = "";
-    for (let i = string.length - 1; i >= 0; i--) {
-        newString += string[i];
-    }
-    
-    if (string === newString){
-        return true 
-    } else {
-        return false
-    }
+    string = string.replace(/[!., ]/g, "").toLowerCase() //regex replace(deletes) !.,(empty space)
+    let reverseStr = string.split("").reverse().join(""); 
+ 
 
+ if (reverseStr == string) {
+    return true
+ } else {
+    return false
+ }
+ console.log(reverseStr)
 };
 
 // Do not edit below this line
