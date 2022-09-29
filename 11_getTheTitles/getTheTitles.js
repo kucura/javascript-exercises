aa2d232f442d07a1ebcb446290e0feeb125e6fbd
kucura <rukicu.gorski@gmail.com>
2022-09-29 20:16:42 +0200

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+let bookTitleArray = []
+const getTheTitles = function(books) {
+ for (i=0; i<books.length; i++) {
+    bookTitleArray.push(books[i].title) //rewrite with .map
 };
-
+return bookTitleArray
+}
 // Do not edit below this line
 module.exports = getTheTitles;
