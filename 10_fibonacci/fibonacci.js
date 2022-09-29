ddf942a4adb3c1299a7f8b7bfbca11bdc3635337
kucura <rukicu.gorski@gmail.com>
2022-09-29 19:39:36 +0200

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
+
+
 const fibonacciArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811]
 const fibonacci = function(i) {
-    return fibonacciArray[i]
+    if (i<0) {
+        return "OOPS"
+    } else if (Number.isInteger(i)) {
+        return fibonacciArray[i]
+    } else {
+        return fibonacciArray[parseInt(i)]
+    }
 };
 
 // Do not edit below this line
